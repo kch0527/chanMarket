d0894b1f942f9798867f65a20dbae18a8dd35ea3
chan <jhgu128@naver.com>
2022-03-29 22:47:43 +0900

    third commit

--- src/main/java/com/example/market/controller/MemberController.java
+++ src/main/java/com/example/market/controller/MemberController.java
@@ -8,13 +8,11 @@ import com.example.market.service.MemberService;
 import com.example.market.session.SessionConst;
 import com.example.market.session.SessionManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/com/example/market/repository/JpaMemberRepository.java
+++ src/main/java/com/example/market/repository/JpaMemberRepository.java
@@ -3,13 +3,14 @@ package com.example.market.repository;
 import com.example.market.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface JpaMemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.email = :email and m.pw = :pw")
-    Member findMember(String email, String pw);
+    Member findMember(@Param("email") String email, @Param("pw") String pw);
 
     @Query("select m from Member m where m.email = :email")
-    Member findByEmail(String email);
+    Member findByEmail(@Param("email") String email);
 
 }
