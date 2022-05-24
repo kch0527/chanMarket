1e69accac7c954e69ac5bfbd9f97fb1a90057357
chan <jhgu128@naver.com>
2022-05-24 17:38:18 +0900

    2022.05.24 commit

--- src/main/java/com/example/market/controller/CommentController.java
+++ src/main/java/com/example/market/controller/CommentController.java
@@ -7,7 +7,6 @@ import com.example.market.service.CommentService;
 import com.example.market.service.ItemService;
 import com.example.market.service.MemberService;
 import lombok.Data;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +14,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.logging.Logger;
 
 @Controller
 @Data
-@RequestMapping( "/chanMarket/itemList")
+@RequestMapping("/chanMarket/itemList")
 public class CommentController {
 
     private final ItemService itemService;
@@ -27,8 +25,7 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("{itemId}")
-    public String itemComment(@PathVariable Long itemId, HttpServletRequest request, @ModelAttribute Comment comment, RedirectAttributes redirectAttributes)
-    {
+    public String itemComment(@PathVariable Long itemId, HttpServletRequest request, @ModelAttribute Comment comment, RedirectAttributes redirectAttributes) {
         try {
             Item item = itemService.readItem(itemId);
             HttpSession session = request.getSession();
@@ -50,30 +47,31 @@ public class CommentController {
     }
 
     @GetMapping("{commentId}/commentEdit")
-    public String commentEdit(@PathVariable Long commentId, Model model){
+    public String commentEdit(@PathVariable Long commentId, Model model) {
         Comment comment = commentService.findComment(commentId);
         model.addAttribute(comment);
         return "comments/commentEdit";
     }
 
     @PutMapping("{commentId}/commentEdit")
-    public String commentEdit(@PathVariable Long commentId, Comment comment){
+    public String commentEdit(@PathVariable Long commentId, Comment comment) {
         commentService.editComment(commentId, comment);
         Comment findComment = commentService.findComment(commentId);
         return "redirect:/chanMarket/itemList/" + findComment.getItem().getId();
 
     }
 
-    @RequestMapping( value = "delete/{commentId}", method = RequestMethod.GET)
-    public String commentDelete(@ModelAttribute Comment comment, @PathVariable Long commentId, RedirectAttributes redirectAttributes, Model model){
-       try {
-           commentService.deleteComment(commentId);
-           redirectAttributes.addFlashAttribute("msg", "삭제 완료");
-       }catch (Exception e){
-           e.printStackTrace();
-           redirectAttributes.addFlashAttribute("msg","오류 발생");
-       }
-        return "redirect:/chanMarket/itemList";
+    @DeleteMapping("delete/{commentId}")
+    public String commentDelete(@ModelAttribute Comment comment, @PathVariable Long commentId, RedirectAttributes redirectAttributes) {
+
+        try {
+            commentService.deleteComment(commentId);
+            redirectAttributes.addFlashAttribute("msg", "삭제 완료");
+        } catch (Exception e) {
+            e.printStackTrace();
+            redirectAttributes.addFlashAttribute("msg", "오류 발생");
+        }
+        return "item/itemList";
     }
 
 }
--- src/main/resources/static/market/js/comment.js
+++ src/main/resources/static/market/js/comment.js
@@ -1,11 +1,18 @@
-$(function() {
-    $("#title li").click(function(e) {
-        e.stopPropagation();
-        var num = $(this).index();
-        $("#modal li").eq(num).show();
-    });
-
-    $(document).click(function() {
-        $("#modal li").hide();
-    })
-})
+// $(document).ready(function() {
+//     var msg = "${msg}";
+//     if(msg != ""){
+//         alert(msg);
+//     }
+// });
+function deleteConfirm(itemId, commentId){
+    if(!confirm("삭제 하시겠습니까?")){
+        return false;
+    }else{
+        const xhr = new XMLHttpRequest();
+        xhr.open("DELETE", `/chanMarket/itemList/delete/${commentId}`, true);
+        xhr.onload = function () {
+            location.href = `/chanMarket/itemList/${itemId}`
+        }
+        xhr.send(null);
+    }
+}
