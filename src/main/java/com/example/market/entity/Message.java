0a7b1a3746cc3f06d3ebaf99f7e14234fcac1435
chan <jhgu128@naver.com>
2022-05-30 21:59:33 +0900

    2022.05.30  1:1 채팅 진행중

---
+++ src/main/java/com/example/market/entity/Message.java
@@ -0,0 +1,32 @@
+package com.example.market.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Message {
+    @Id
+    @GeneratedValue
+    @Column(name = "message_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private ChatRoom chatRoom;
+
+    @Size(max = 30)
+    @NotNull
+    private String text;
+
+    private String nowTime;
+}
