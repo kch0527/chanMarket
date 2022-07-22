1dbfab4a6956340d105843c8e3573fa86a871431
chan <jhgu128@naver.com>
2022-07-22 17:16:59 +0900

    2022.07.22  join, login 폼 수정, css 추가

--- src/main/java/com/example/market/config/SocketConfig.java
+++ src/main/java/com/example/market/config/SocketConfig.java
@@ -1,6 +1,6 @@
 package com.example.market.config;
 
-
+/*
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
 
@@ -14,3 +14,4 @@ public class SocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/").setAllowedOrigins("*").withSockJS(); //양 사용자 간 웹소켓 핸드 셰이크를 위해 지정
     }
 }
+*/
